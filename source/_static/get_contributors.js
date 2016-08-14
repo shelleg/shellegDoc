d89f8b8e8ecd8efab2f22c1d7f5cea8485322c24
Andrey Shuster <novus42@gmail.com>
2016-08-14 14:57:33 +0300

    adding commits and repos data

--- source/_static/get_contributors.js
+++ source/_static/get_contributors.js
@@ -9,53 +9,85 @@
     var container = $("#contributors_dynamic");
     var contributors_table = $('#contributors_data');
 
+    var repos_stats = [];
+    var users_stats = [];
+
+    var repos_ready = false;
+    var users_ready = false;
+
     $.get(ORG_REPOS_URL)
         .done(function (data) {
-            renderStats(data);
+            fetchStats(data);
+            repos_ready = true;
         });
 
     $.get(TEAM_MEMBERS_URL)
         .done(function (data) {
-            renderMembers(data);
+            users_stats = data;
+            users_ready = true;
         });
 
-    function getSingleUser(username, callback) {
-        $.get(ROOT_URL + "/users/" + username + params)
-            .done(function (data) {
-                callback(data);
-            });
+    function fetchStats(data) {
+        $.each(data, function (index, item) {
+            $.get(ROOT_URL + "/repos/shelleg/" + item.name + "/stats/contributors" + params)
+                .done(function (data) {
+                    repos_stats.push({ repo_name: item.name, repo_data: data });
+                });
+        });
     }
 
-    function renderStats(data) {
-        $.each(data, function (index, item) {
-            $.get(ROOT_URL + "/repos/shelleg/" + item + "/stats/contributors" + params)
-                .done(function (item) {
-                    console.log(item);
-                });
+    var renderData = setInterval(function () {
+        if (users_ready && repos_ready) {
+            clearInterval(renderData);
+            renderMembers(users_stats);
+            renderStats(repos_stats);
+
+        }
+    }, 1000);
+
+    function renderStats(repos_stats) {
+        $.each(repos_stats, function (index, repo) {
+            var repo_name = repo.repo_name;
+            var repo_data = repo.repo_data;
+            console.log(repo_data);
+            $.each(repo_data, function (i, data) {
+                $("td[data-login='" + data.author.login + "']").append(repo_name + "<br>");
+                $("td[data-login='" + data.author.login + "']").next().append(data.total + "<br>");
+            });
         });
     }
 
     function renderMembers(data) {
         $.each(data, function (index, item) {
             var tr = $('<tr>');
-            tr.data('login', item.login);
-
             var avatar_img = $("<img>", { src: item.avatar_url, class: "github_avatar" });
             var avatar_td = $("<td>").html(avatar_img);
 
             var login_link = "<a href=" + item.html_url + ">" + item.login + "</a>";
             var username_td = $('<td>').html(login_link);
 
-            var first_name = "";
+            tr.append(avatar_td)
+                .append(username_td)
+                .append($('<td>'))
+                .append($('<td>', { 'data-login': item.login }))
+                .append($('<td>'));
 
-            getSingleUser(item.login, function (user_data) {
-                first_name = $('<td>').html(user_data.name);
-                tr.append(avatar_td)
-                    .append(username_td)
-                    .append(first_name);
-            });
+            // getSingleUser(item.login, function (user_data) {
+            //     first_name = $('<td>').html(user_data.name);
+            //     tr.append(avatar_td)
+            //         .append(username_td)
+            //         .append(first_name)
+            //         .append($('<td>', { 'data-login': item.login }));
+            // });
             contributors_table.append(tr);
         });
+    }
+
+    function getSingleUser(username, callback) {
+        $.get(ROOT_URL + "/users/" + username + params)
+            .done(function (data) {
+                callback(data);
+            });
     }
 
 })($);
